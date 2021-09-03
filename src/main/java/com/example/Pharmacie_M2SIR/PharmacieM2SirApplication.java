32eb7daf43ae371e35a28ce44de6254a740d6fed
Djibril-dudo <diouf920410@gmail.com>
2021-09-03 22:45:20 +0200

    premier commit du projet

---
+++ src/main/java/com/example/Pharmacie_M2SIR/PharmacieM2SirApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pharmacie_M2SIR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacieM2SirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacieM2SirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pharmacie_M2SIR/PharmacieM2SirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pharmacie_M2SIR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacieM2SirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
